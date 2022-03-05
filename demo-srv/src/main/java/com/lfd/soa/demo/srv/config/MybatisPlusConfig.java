91087af97744f5c9862fd9e65c2e0c0adc94f510
林丰达 <fengda.lin@patpat.com>
2022-03-05 23:30:23 +0800

    refactor: 提交

--- demo-srv/src/main/java/com/lfd/soa/demo/srv/config/MybatisPlusConfig.java
+++ demo-srv/src/main/java/com/lfd/soa/demo/srv/config/MybatisPlusConfig.java
@@ -8,29 +8,32 @@ import com.lfd.soa.demo.srv.support.mybatis.MybatisResultInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * mybatis plus配置
- * @author linfengda
- * @date 2021-01-21 17:14
+ * @ClassName: MybatisPlusConfig
+ * @Description: mybatisPlus配置
+ * @date: 2018/1/18
+ * @author heweiming
+ * 1.0
  */
-@EnableTransactionManagement
 @Configuration
-@MapperScan({"com.lfd.soa.demo.srv.mapper"})
+@MapperScan({"com.patpat.msp.**.mapper"})
 public class MybatisPlusConfig {
 
     /**
-     * 分页插件，乐观锁插件
-     */
+     * @Author mochengqi
+     * @Description mybatis plus 自带分页插件,一缓和二缓遵循mybatis的规则
+     * @Date 16:23 2021/8/18
+     **/
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
         // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
         // paginationInnerInterceptor.setOverflow(false);
+
         // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInnerInterceptor.setMaxLimit(1000L);
+        paginationInnerInterceptor.setMaxLimit(500L);
         interceptor.addInnerInterceptor(paginationInnerInterceptor);
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return interceptor;
@@ -40,7 +43,7 @@ public class MybatisPlusConfig {
      * 结果集拦截插件
      */
     @Bean
-    public MybatisResultInterceptor mesResultInterceptor() {
+    public MybatisResultInterceptor mspResultInterceptor() {
         return new MybatisResultInterceptor();
     }
 }
